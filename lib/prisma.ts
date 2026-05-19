b9cf05df6f9dce54b3fb9df58678125430ec9122
davilepore_ <davilepore218@gmail.com>
2026-05-19 20:36:44 -0300

    chore: change settings

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,13 @@
-// lib/prisma.ts
 import { PrismaClient } from '@/app/generated/prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL não está definida')
+}
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma =
   globalForPrisma.prisma ?? new PrismaClient({ adapter })
